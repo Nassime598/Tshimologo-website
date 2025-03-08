803dd65d918da0201107ee25be15ca5a214e163c
Nassime598 <nassimeel150@gmail.com>
2025-03-08 13:57:05 +0000

    second push

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/Tshimologo-website/', // Set this to your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
